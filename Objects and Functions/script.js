d0168c883fa12d1b0901acb0d72fd8afe48d5604
akhilnalla <arunreddy096@gmail.com>
2020-04-12 10:36:42 +0530

    added conent related to budget

--- Objects and Functions/script.js
+++ Objects and Functions/script.js
@@ -173,6 +173,53 @@
 // console.log(designerQuestion)
 
 
+/////////////////////////////
+// Lecture: IIFE
+
+// function game() {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5);
+// }
+// game();
+// (function () {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5);
+// })();
+// console.log(score);
+// (function (goodLuck) {
+//     var score = Math.random() * 10;
+//     console.log(score >= 5 - goodLuck);
+// })(5);
+
+/////////////////
+// Lecture : closure
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
